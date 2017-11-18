c1275cf7454266fcd0ec211853790a30fd3776ee
inventive-seven <mr.kakorin@yandex.ru>
2017-11-18 19:54:23 +0300

    fix db

--- src/GameInformationAPI.js
+++ src/GameInformationAPI.js
@@ -1,4 +1,17 @@
-const mysql = require("mysql2/promise");
+const db = require("./lib/db");
 
+const DBStateSaveBehaviourTargets = ['heroes', 'objects'];
+
+const move = (target) =>{
+	if (DBStateSaveBehaviourTargets.indexOf(target.moveTargetType)!=-1){
+		let object = {
+			id:target.moveTargetId,
+			type:target.moveTargetType,
+		}
+	}
+	else{
+
+	}
+}
 
 module.exports ={move:move, attack:attack, use:use}
